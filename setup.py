1c943681a659aeed0ec844d317d191e5804ab61f
itick <itick100@gmail.com>
2026-04-25 23:23:18 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type="text/markdown",
     author="iTick",
     author_email="support@itick.org",
-    url="https://github.com/itick/sdk",
+    url="https://github.com/itick-org/python-sdk",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
